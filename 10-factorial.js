c74acd1d45dc64ddb717e864effc12cc1562adae
missuniverse5357 <kamaulydia809@gmail.com>
2025-11-11 02:24:58 +0300

    Add files via upload

---
+++ 10-factorial.js
@@ -0,0 +1,10 @@
+function factorial(n) {
+    if (isNaN(n) || n <= 1) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+const args = process.argv.slice(2);
+const num = parseInt(args[0]);
+console.log(factorial(num));
